5ba05eb4a861e418288c43e175bd078814297bc6
aperkaz <alainperkaz@gmail.com>
2017-04-25 00:41:15 +0200

    Fixed enzyme PropTypes warning

--- app/components/Example/Example.jsx
+++ app/components/Example/Example.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Example = ({ children }) => (
   <h1>This is example. So say: {children}</h1>
