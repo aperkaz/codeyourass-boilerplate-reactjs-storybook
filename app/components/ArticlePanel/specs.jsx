cd99f82057f16dbc009075b763d638dc4a11c21a
aperkaz <alainperkaz@gmail.com>
2017-04-25 21:36:40 +0200

    Added preliminary test cases

--- app/components/ArticlePanel/specs.jsx
+++ app/components/ArticlePanel/specs.jsx
@@ -1,31 +1,88 @@
 import React from 'react';
 import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
-import { shallow } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import ArticlePanel from './ArticlePanel';
 
-/*
+/* Props for tests */
 const defaultProps = {
+  dispatch: () => {},
+  manifest: { components: [] },
+};
+
+const baseProps = {
+  title: 'BASE TITLE',
+  subtitle: '',
+  followers: 1,
+  userName: 'ALAIN PERKAZ',
+  date: '1 MARCH 2017',
+  imagePath: '',
+};
+
+const imageProps = {
   title: 'TITLE',
-  subtitle: 'SUBTITLE. DOLOR AMET DOLO AMET',
-  followers: 15,
-  userName: 'DENISA JUNA',
-  date: '3 MARCH 2017',
+  subtitle: '',
+  followers: 2,
+  userName: 'ALAIN PERKAZ',
+  date: '1 MARCH 2017',
   imagePath: 'http://kingofwallpapers.com/gradient/gradient-011.jpg',
 };
-*/
+
+const subtitleProps = {
+  title: 'TITLE',
+  subtitle: 'SUBTITLE',
+  followers: 3,
+  userName: 'ALAIN PERKAZ',
+  date: '1 MARCH 2017',
+  imagePath: '',
+};
+
+const completeProps = {
+  title: 'COMPLETE TITLE',
+  subtitle: 'COMPLETE SUBTITLE',
+  followers: 4,
+  userName: 'ALAIN PERKAZ',
+  date: '1 MARCH 2017',
+  imagePath: 'http://kingofwallpapers.com/gradient/gradient-011.jpg',
+};
 
 describe('ArticlePanel', () => {
-  it('should contain div', () => {
-    const wrapper = shallow(<ArticlePanel />);
-    expect(wrapper.find('div')).be.present();
+  it('should render without blowing up', () => {
+    const wrapper = shallow(<ArticlePanel {...defaultProps} />);
+    expect(wrapper.length).to.eql(1);
   });
-  
+
   // assert the content of the new createt component
   it('should contain div', () => {
-    const component = shallow(<ArticlePanel />);
-    expect(component.find('div')).be.present();
+    const wrapper = shallow(<ArticlePanel />);
+    expect(wrapper.find('#main')).be.present();
   });
+
+  // assert props render correctly
+  it('rendering with image and subtitle', () => {
+    const wrapper = mount(<ArticlePanel data={completeProps} />);
+
+    expect(wrapper.find('#title').text()).to.equal('COMPLETE TITLE');
+    expect(wrapper.find('#subtitle').text()).to.equal('COMPLETE SUBTITLE');
+    expect(wrapper.find('#followers').text()).to.equal('4 FOLLOWERS');
+    expect(wrapper.find('#userName').text()).to.equal('ALAIN PERKAZ');
+    expect(wrapper.find('#date').text()).to.equal('1 MARCH 2017');
+    expect(wrapper.find('#main').props().style.backgroundImage).to.eql('url( http://kingofwallpapers.com/gradient/gradient-011.jpg )');
+  });
+
+  it('rendering with subtitle', () => {
+    //const wrapper = shallow(<ArticlePanel props={subtitleProps}/>);
+    //expect(wrapper).to.containMatchingElement(  <h4 id="subtitle">SUBTITLE</h4>);
+  });
+
+  it('rendering with image', () => {
+
+  });
+
+  it('rendering without image and subtitle', () => {
+
+  });
+
 });
 
 chai.use(chaiEnzyme());
